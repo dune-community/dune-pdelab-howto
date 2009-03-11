0657d48392138b17d6b0a6efa78968b352d74aed
Peter Bastian <peter@dune-project.org>
2009-03-11 06:45:32 +0000

    q2 interpolation error
    Imported from SVN: r18 at trunk

---
+++ examples/q1constraints.hh
@@ -0,0 +1,29 @@
+class Q1Constraints
+{
+public:
+  enum { doBoundary = true };
+  enum { doSkeleton = false };
+  enum { doVolume = false };
+
+  template<typename B, typename I, typename LFS, typename T>
+  void boundary (const B& b, const IntersectionGeometry<I>& ig, 
+				 const LFS& lfs, T& trafo) const
+  {
+	// 2D here, get midpoint of edge
+	typename F::Traits::DomainType ip(0.5);
+
+	// determine type of boundary condition
+	typename F::Traits::RangeType bctype;
+	b.evaluate(ig,ip,bctype);
+
+	// >0 means Dirichlet boundary
+	if (bctype<=0) return;
+
+	// the two end nodes of the edge are constrained
+	typename T::RowType empty;
+	if (ig.numberInSelf()==0) { trafo[0] = empty; trafo[2] = empty; }
+	if (ig.numberInSelf()==1) { trafo[1] = empty; trafo[3] = empty; }
+	if (ig.numberInSelf()==2) { trafo[0] = empty; trafo[1] = empty; }
+	if (ig.numberInSelf()==3) { trafo[2] = empty; trafo[3] = empty; }
+  }
+};
