2a76ff1dec457196557136a353b2359a7b9b76df
Peter Bastian <peter@dune-project.org>
2009-03-17 22:34:54 +0000

    praktisch fertig
    Imported from SVN: r30 at trunk

--- examples/laplacedirichlet.hh
+++ examples/laplacedirichlet.hh
@@ -20,12 +20,12 @@ template<typename GV, typename FEM, typename CON>
 void laplacedirichlet (const GV& gv, const FEM& fem, 
 					   int qorder, std::string filename)
 {
-  typedef typename GV::Grid::ctype D; // domain type
+  typedef typename GV::Grid::ctype D; // domain type                  /*@\label{lapdriver:FirstKnown}@*/
   typedef double R;                   // range type
 
   // set up grid function space
   typedef Dune::PDELab::GridFunctionSpace<GV,FEM,CON,
-	Dune::PDELab::ISTLVectorBackend<1> > GFS; // Use ISTL Vector    
+	Dune::PDELab::ISTLVectorBackend<1> > GFS; // Use ISTL Vector      /*@\label{lapdriver:ISTLBackend}@*/
   GFS gfs(gv,fem);                    // make grid function space
 
   typedef typename GFS::template ConstraintsContainer<R>::Type T;
@@ -36,53 +36,51 @@ void laplacedirichlet (const GV& gv, const FEM& fem,
   typedef typename GFS::template VectorContainer<R>::Type X;
   X x(gfs,0.0);                       // make coefficient vector
 
-  U<GV,R> u(gv);                      // make analytic function object
+  U<GV,R> u(gv);                      // analytic function object
   Dune::PDELab::interpolate(u,gfs,x); // interpolate x from u
-  Dune::PDELab::set_nonconstrained_dofs(t,0.0,x); // clear interior
+  Dune::PDELab::set_nonconstrained_dofs(t,0.0,x); // clear interior  /*@\label{lapdriver:LastKnown}@*/
 
   // set up operator
-  LaplaceDirichlet lop(qorder);       // local operator
+  LaplaceDirichlet lop(qorder);       // local operator              /*@\label{lapdriver:ResEval}@*/
   typedef Dune::PDELab::GridOperatorSpace<GFS,GFS,
     LaplaceDirichlet,T,T,
-	Dune::PDELab::ISTLBCRSMatrixBackend<1,1> > GOS;
-  GOS gos(gfs,t,gfs,t,lop);           // global operator
+	Dune::PDELab::ISTLBCRSMatrixBackend<1,1> > GOS;                  /*@\label{lapdriver:GOS}@*/
+  GOS gos(gfs,t,gfs,t,lop);           // global operator 
 
   // solve problem with matrix
-  typedef typename GOS::template MatrixContainer<R>::Type M;
-  M m(gos);                           // matrix representation
-  m = 0.0;                            // clear matrix
-  gos.jacobian(x,m);                  // assemble stiffness matrix
+  typedef typename GOS::template MatrixContainer<R>::Type M;         /*@\label{lapdriver:MatrixType}@*/
+  M m(gos); m = 0.0;                  // matrix representation       /*@\label{lapdriver:MatrixSetup}@*/
+  gos.jacobian(x,m);                  // assemble stiffness matrix   /*@\label{lapdriver:JacoAssemble}@*/
 
-  Dune::MatrixAdapter<M,X,X> op(m);   // ISTL operator from matrix
+  Dune::MatrixAdapter<M,X,X> op(m);   // ISTL operator from matrix   /*@\label{lapdriver:ISTLFirst}@*/
   Dune::SeqSSOR<M,X,X> ssor(m,1,1.0); // a preconditioner
   Dune::CGSolver<X> solver(op,ssor,1E-10,5000,2); // CG solver
-  Dune::InverseOperatorResult stat;   // status report of solver
+  Dune::InverseOperatorResult stat;   // status report of solver     /*@\label{lapdriver:ISTLLast}@*/
 
-  X r(gfs,0.0);                       // residual vector
-  gos.residual(x,r);                  // compute residual
+  X r(gfs,0.0);                       // residual vector             
+  gos.residual(x,r);                  // compute residual            /*@\label{lapdriver:Residual}@*/
   X z(gfs,0.0);                       // update vector
-  solver.apply(z,r,stat);             // solve for update 
-  x -= z;                             // apply update
+  solver.apply(z,r,stat);             // solve for update            /*@\label{lapdriver:UpdateEq}@*/
+  x -= z;                             // apply update                /*@\label{lapdriver:Update}@*/
 
   // solve problem without matrix ("on the fly")
-  Dune::PDELab::OnTheFlyOperator<X,X,GOS> opb(gos);// operator apply
-  Dune::Richardson<X,X> richardson(1.0);           // no precond.
-  Dune::CGSolver<X> solverb(opb,richardson,1E-10,5000,2); // CG
+  Dune::PDELab::OnTheFlyOperator<X,X,GOS> opb(gos);// operator apply /*@\label{lapdriver:OnTheFlyOp}@*/
+  Dune::Richardson<X,X> richardson(1.0);           // no precond.    /*@\label{lapdriver:Richardson}@*/
+  Dune::CGSolver<X> solverb(opb,richardson,1E-10,5000,2); // CG      /*@\label{lapdriver:NewCG}@*/
   
-  Dune::PDELab::interpolate(u,gfs,x); // reinitialize x
+  Dune::PDELab::interpolate(u,gfs,x); // reinitialize x              /*@\label{lapdriver:AltSolverFirst}@*/
   Dune::PDELab::set_nonconstrained_dofs(t,0.0,x);  // clear interior
   r = 0.0;                            // clear residual
   gos.residual(x,r);                  // compute residual
   z = 0.0;                            // clear update
   solverb.apply(z,r,stat);            // solve for update 
-  x -= z;                             // apply update
+  x -= z;                             // apply update                /*@\label{lapdriver:AltSolverLast}@*/
 
   // show result
-  typedef Dune::PDELab::DiscreteGridFunction<GFS,X> DGF;
+  typedef Dune::PDELab::DiscreteGridFunction<GFS,X> DGF;             /*@\label{lapdriver:VTKFirst}@*/
   DGF dgf(gfs,x);                     // make a grid function
-
   Dune::SubsamplingVTKWriter<GV> vtkwriter(gv,2); // plot result
   vtkwriter.addVertexData(new Dune::PDELab::
 						  VTKGridFunctionAdapter<DGF>(dgf,"u"));
-  vtkwriter.write(filename,Dune::VTKOptions::ascii); 
+  vtkwriter.write(filename,Dune::VTKOptions::ascii);                 /*@\label{lapdriver:VTKLast}@*/
 }
--- examples/laplacedirichletop.hh
+++ examples/laplacedirichletop.hh
@@ -7,13 +7,13 @@
 #include<dune/pdelab/localoperator/pattern.hh>
 
 class LaplaceDirichlet
-  : public Dune::PDELab::NumericalJacobianApplyVolume<LaplaceDirichlet>,
-	public Dune::PDELab::NumericalJacobianVolume<LaplaceDirichlet>,
-	public Dune::PDELab::FullVolumePattern
+: public Dune::PDELab::NumericalJacobianApplyVolume<LaplaceDirichlet>, /*@\label{laplace:JacApply}@*/
+  public Dune::PDELab::NumericalJacobianVolume<LaplaceDirichlet>,      /*@\label{laplace:Jac}@*/
+  public Dune::PDELab::FullVolumePattern                               /*@\label{laplace:Pattern}@*/
 {
 public:
   // pattern assembly flags
-  enum { doPatternVolume = true };
+  enum { doPatternVolume = true };    /*@\label{laplace:FirstFlag}@*/
   enum { doPatternSkeleton = false };
 
   // residual assembly flags
@@ -22,13 +22,15 @@ public:
   enum { doAlphaBoundary = false };
   enum { doLambdaVolume = false };
   enum { doLambdaSkeleton = false };
-  enum { doLambdaBoundary = false };
+  enum { doLambdaBoundary = false };  /*@\label{laplace:LastFlag}@*/
 
   LaplaceDirichlet (int qorder_) : qorder(qorder_) {}
 
   // volume integral depending on test and ansatz functions
-  template<typename EG, typename LFSU, typename X, typename LFSV, typename R>
-  void alpha_volume (const EG& eg, const LFSU& lfsu, const X& x, const LFSV& lfsv, R& r) const
+  template<typename EG, typename LFSU, typename X, 
+		   typename LFSV, typename R>
+  void alpha_volume (const EG& eg, const LFSU& lfsu, const X& x, /*@\label{laplace:AlphaVolume}@*/
+					 const LFSV& lfsv, R& r) const
   {
 	// domain and range field type
 	typedef typename LFSU::Traits::LocalFiniteElementType::
@@ -44,17 +46,21 @@ public:
 
 	// select quadrature rule
 	Dune::GeometryType gt = eg.geometry().type();
-	const Dune::QuadratureRule<DF,dim>& rule = Dune::QuadratureRules<DF,dim>::rule(gt,qorder);
+	const Dune::QuadratureRule<DF,dim>& 
+	  rule = Dune::QuadratureRules<DF,dim>::rule(gt,qorder);
 
 	// loop over quadrature points
-	for (typename Dune::QuadratureRule<DF,dim>::const_iterator it=rule.begin(); it!=rule.end(); ++it)
+	for (typename Dune::QuadratureRule<DF,dim>::const_iterator 
+		   it=rule.begin(); it!=rule.end(); ++it)
 	  {
-		// evaluate gradient of shape functions (we assume Galerkin method lfsu=lfsv)
+		// evaluate gradient of shape functions (assume Galerkin)
 		std::vector<JacobianType> js(lfsu.size());
-		lfsu.localFiniteElement().localBasis().evaluateJacobian(it->position(),js);
+		lfsu.localFiniteElement().localBasis().evaluateJacobian(
+		  it->position(),js);
 
 		// transform gradient to real element
-		const Dune::FieldMatrix<DF,dimw,dim> jac = eg.geometry().jacobianInverseTransposed(it->position());
+		const Dune::FieldMatrix<DF,dimw,dim> jac = 
+		  eg.geometry().jacobianInverseTransposed(it->position());
 		std::vector<Dune::FieldVector<RF,dim> > gradphi(lfsu.size());
 		for (int i=0; i<lfsu.size(); i++)
 		  {
@@ -68,7 +74,8 @@ public:
 		  gradu.axpy(x[i],gradphi[i]);
 
 		// integrate grad u * grad phi_i
-		RF factor = it->weight() * eg.geometry().integrationElement(it->position());
+		RF factor = it->weight() * eg.geometry().integrationElement(
+		                                          it->position());
 		for (int i=0; i<lfsu.size(); i++)
 		  r[i] += (gradu*gradphi[i])*factor;
 	  }
