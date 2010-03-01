cd0cb9b87d01eacc953cc15a3478993f3c661fe0
Peter Bastian <peter@dune-project.org>
2010-03-01 14:56:41 +0000

    first example for DUNE course
    Imported from SVN: r133 at trunk

---
+++ examples/example01.cc
@@ -0,0 +1,533 @@
+// -*- tab-width: 4; indent-tabs-mode: nil -*-
+#ifdef HAVE_CONFIG_H
+#include "config.h"     
+#endif
+#include<math.h>
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+
+#include<dune/common/mpihelper.hh>
+#include<dune/common/exceptions.hh>
+#include<dune/common/fvector.hh>
+#include<dune/common/static_assert.hh>
+#include<dune/common/timer.hh>
+#include<dune/grid/io/file/vtk/subsamplingvtkwriter.hh>
+#include<dune/grid/io/file/gmshreader.hh>
+#include<dune/grid/yaspgrid.hh>
+#if HAVE_ALBERTA
+#include<dune/grid/albertagrid.hh>
+#include <dune/grid/albertagrid/dgfparser.hh>
+#endif
+#if HAVE_UG 
+#include<dune/grid/uggrid.hh>
+#endif
+#if HAVE_ALUGRID
+#include<dune/grid/alugrid.hh>
+#include<dune/grid/io/file/dgfparser/dgfalu.hh>
+#include<dune/grid/io/file/dgfparser/dgfparser.hh>
+#endif
+#include<dune/istl/bvector.hh>
+#include<dune/istl/operators.hh>
+#include<dune/istl/solvers.hh>
+#include<dune/istl/preconditioners.hh>
+#include<dune/istl/io.hh>
+#include<dune/istl/superlu.hh>
+
+#include<dune/pdelab/finiteelementmap/p0fem.hh>
+#include<dune/pdelab/finiteelementmap/p12dfem.hh>
+#include<dune/pdelab/finiteelementmap/pk2dfem.hh>
+#include<dune/pdelab/finiteelementmap/pk3dfem.hh>
+#include<dune/pdelab/finiteelementmap/q12dfem.hh>
+#include<dune/pdelab/finiteelementmap/q22dfem.hh>
+#include<dune/pdelab/finiteelementmap/q1fem.hh>
+#include<dune/pdelab/finiteelementmap/p1fem.hh>
+#include<dune/pdelab/finiteelementmap/rannacher_turek2dfem.hh>
+#include<dune/pdelab/finiteelementmap/conformingconstraints.hh>
+#include<dune/pdelab/gridfunctionspace/gridfunctionspace.hh>
+#include<dune/pdelab/gridfunctionspace/gridfunctionspaceutilities.hh>
+#include<dune/pdelab/gridfunctionspace/genericdatahandle.hh>
+#include<dune/pdelab/gridfunctionspace/interpolate.hh>
+#include<dune/pdelab/gridfunctionspace/constraints.hh>
+#include<dune/pdelab/common/function.hh>
+#include<dune/pdelab/common/vtkexport.hh>
+#include<dune/pdelab/gridoperatorspace/gridoperatorspace.hh>
+#include<dune/pdelab/gridoperatorspace/gridoperatorspaceutilities.hh>
+#include<dune/pdelab/backend/istlvectorbackend.hh>
+#include<dune/pdelab/backend/istlmatrixbackend.hh>
+#include<dune/pdelab/backend/istlsolverbackend.hh>
+#include<dune/pdelab/stationary/linearproblem.hh>
+
+#include"gridexamples.hh"
+
+//===============================================================
+// Local operator
+//===============================================================
+
+#include<dune/grid/common/genericreferenceelements.hh>
+#include<dune/grid/common/quadraturerules.hh>
+#include<dune/pdelab/common/geometrywrapper.hh>
+#include<dune/pdelab/localoperator/pattern.hh>
+#include<dune/pdelab/localoperator/flags.hh>
+
+/** a local operator for solving the equation
+ *
+ *   - \Delta u + u   = exp(||x||^2)   in \Omega
+ *   \nabla u \cdot n = 0              on \partial\Omega
+ *
+ * with conforming finite elements on all types of grids in any dimension
+ *
+ */
+class Example01LocalOperator : 
+  public Dune::PDELab::NumericalJacobianApplyVolume<Example01LocalOperator>,
+  public Dune::PDELab::NumericalJacobianVolume<Example01LocalOperator>,
+  public Dune::PDELab::FullVolumePattern,
+  public Dune::PDELab::LocalOperatorDefaultFlags
+{
+public:
+  // pattern assembly flags
+  enum { doPatternVolume = true };
+
+  // residual assembly flags
+  enum { doAlphaVolume = true };
+
+  Example01LocalOperator (unsigned int intorder_=2)
+    : intorder(intorder_)
+  {}
+
+  // volume integral depending on test and ansatz functions
+  template<typename EG, typename LFSU, typename X, typename LFSV, typename R>
+  void alpha_volume (const EG& eg, const LFSU& lfsu, const X& x, const LFSV& lfsv, R& r) const
+  {
+    // extract some types
+    typedef typename LFSU::Traits::LocalFiniteElementType::
+      Traits::LocalBasisType::Traits::DomainFieldType DF;
+    typedef typename LFSU::Traits::LocalFiniteElementType::
+      Traits::LocalBasisType::Traits::RangeFieldType RF;
+    typedef typename LFSU::Traits::LocalFiniteElementType::
+      Traits::LocalBasisType::Traits::JacobianType JacobianType;
+    typedef typename LFSU::Traits::LocalFiniteElementType::
+      Traits::LocalBasisType::Traits::RangeType RangeType;
+    typedef typename LFSU::Traits::SizeType size_type;
+        
+    // dimensions
+    const int dim = EG::Geometry::dimension;
+    const int dimw = EG::Geometry::dimensionworld;
+
+    // select quadrature rule
+    Dune::GeometryType gt = eg.geometry().type();
+    const Dune::QuadratureRule<DF,dim>& rule = Dune::QuadratureRules<DF,dim>::rule(gt,intorder);
+
+    // loop over quadrature points
+    for (typename Dune::QuadratureRule<DF,dim>::const_iterator 
+           it=rule.begin(); it!=rule.end(); ++it)
+      {
+        // evaluate basis functions on reference element
+        std::vector<RangeType> phi(lfsu.size());
+        lfsu.localFiniteElement().localBasis().evaluateFunction(it->position(),phi);
+
+        // compute u at integration point
+        RF u=0.0;
+        for (size_type i=0; i<lfsu.size(); i++)
+          u += x[i]*phi[i];
+
+        // evaluate gradient of basis functions on reference element
+        std::vector<JacobianType> js(lfsu.size());
+        lfsu.localFiniteElement().localBasis().evaluateJacobian(it->position(),js);
+
+        // transform gradients from reference element to real element
+        const Dune::FieldMatrix<DF,dimw,dim> 
+          jac = eg.geometry().jacobianInverseTransposed(it->position());
+        std::vector<Dune::FieldVector<RF,dim> > gradphi(lfsu.size());
+        for (size_type i=0; i<lfsu.size(); i++)
+          jac.mv(js[i][0],gradphi[i]);
+        
+        // compute gradient of u
+        Dune::FieldVector<RF,dim> gradu(0.0);
+        for (size_type i=0; i<lfsu.size(); i++)
+          gradu.axpy(x[i],gradphi[i]);
+
+        // evaluate parameters; 
+        Dune::FieldVector<RF,dim> 
+          globalpos = eg.geometry().global(it->position());
+        RF f = exp(globalpos.two_norm2()); 
+        RF a =  1.0; 
+
+        // integrate grad u * grad phi_i + a*u*phi_i - f phi_i
+        RF factor = it->weight()*eg.geometry().integrationElement(it->position());
+        for (size_type i=0; i<lfsu.size(); i++)
+          r[i] += ( gradu*gradphi[i] + a*u*phi[i] - f*phi[i] )*factor;
+      }
+  }
+
+private:
+  unsigned int intorder;
+};
+
+
+/** a local operator for solving the equation
+ *
+ *   - \Delta u + u   = exp(u)   in \Omega
+ *   \nabla u \cdot n = 0        on \partial\Omega
+ *
+ * with conforming finite elements on all types of grids in any dimension
+ *
+ */
+class Example01bLocalOperator : 
+  public Dune::PDELab::NumericalJacobianApplyVolume<Example01LocalOperator>,
+  public Dune::PDELab::NumericalJacobianVolume<Example01LocalOperator>,
+  public Dune::PDELab::FullVolumePattern,
+  public Dune::PDELab::LocalOperatorDefaultFlags
+{
+public:
+  // pattern assembly flags
+  enum { doPatternVolume = true };
+
+  // residual assembly flags
+  enum { doAlphaVolume = true };
+
+  Example01bLocalOperator (unsigned int intorder_=2)
+    : intorder(intorder_)
+  {}
+
+  // volume integral depending on test and ansatz functions
+  template<typename EG, typename LFSU, typename X, typename LFSV, typename R>
+  void alpha_volume (const EG& eg, const LFSU& lfsu, const X& x, const LFSV& lfsv, R& r) const
+  {
+    // extract some types
+    typedef typename LFSU::Traits::LocalFiniteElementType::
+      Traits::LocalBasisType::Traits::DomainFieldType DF;
+    typedef typename LFSU::Traits::LocalFiniteElementType::
+      Traits::LocalBasisType::Traits::RangeFieldType RF;
+    typedef typename LFSU::Traits::LocalFiniteElementType::
+      Traits::LocalBasisType::Traits::JacobianType JacobianType;
+    typedef typename LFSU::Traits::LocalFiniteElementType::
+      Traits::LocalBasisType::Traits::RangeType RangeType;
+    typedef typename LFSU::Traits::SizeType size_type;
+        
+    // dimensions
+    const int dim = EG::Geometry::dimension;
+    const int dimw = EG::Geometry::dimensionworld;
+
+    // select quadrature rule
+    Dune::GeometryType gt = eg.geometry().type();
+    const Dune::QuadratureRule<DF,dim>& rule = Dune::QuadratureRules<DF,dim>::rule(gt,intorder);
+
+    // loop over quadrature points
+    for (typename Dune::QuadratureRule<DF,dim>::const_iterator 
+           it=rule.begin(); it!=rule.end(); ++it)
+      {
+        // evaluate basis functions on reference element
+        std::vector<RangeType> phi(lfsu.size());
+        lfsu.localFiniteElement().localBasis().evaluateFunction(it->position(),phi);
+
+        // compute u at integration point
+        RF u=0.0;
+        for (size_type i=0; i<lfsu.size(); i++)
+          u += x[i]*phi[i];
+
+        // evaluate gradient of basis functions on reference element
+        std::vector<JacobianType> js(lfsu.size());
+        lfsu.localFiniteElement().localBasis().evaluateJacobian(it->position(),js);
+
+        // transform gradients from reference element to real element
+        const Dune::FieldMatrix<DF,dimw,dim> 
+          jac = eg.geometry().jacobianInverseTransposed(it->position());
+        std::vector<Dune::FieldVector<RF,dim> > gradphi(lfsu.size());
+        for (size_type i=0; i<lfsu.size(); i++)
+          jac.mv(js[i][0],gradphi[i]);
+        
+        // compute gradient of u
+        Dune::FieldVector<RF,dim> gradu(0.0);
+        for (size_type i=0; i<lfsu.size(); i++)
+          gradu.axpy(x[i],gradphi[i]);
+
+        // evaluate parameters; 
+        Dune::FieldVector<RF,dim> 
+          globalpos = eg.geometry().global(it->position());
+        RF f = exp(u); 
+        RF a =  1.0; 
+
+        // integrate grad u * grad phi_i + a*u*phi_i - f phi_i
+        RF factor = it->weight()*eg.geometry().integrationElement(it->position());
+        for (size_type i=0; i<lfsu.size(); i++)
+          r[i] += ( gradu*gradphi[i] + a*u*phi[i] - f*phi[i] )*factor;
+      }
+  }
+
+private:
+  unsigned int intorder;
+};
+
+//===============================================================
+// Some variants to solve the nonlinear diffusion problem
+//===============================================================
+
+// a sequential variant
+template<class GV>
+void example01_sequential_Q1 (const GV& gv)
+{
+  // <<<1>>> Choose domain and range field type
+  typedef typename GV::Grid::ctype Coord;
+  typedef double Real;
+  const int dim = GV::dimension;
+
+  // <<<2a>>> Make grid function space
+  typedef Dune::PDELab::Q1LocalFiniteElementMap<Coord,Real,dim> FEM;
+  FEM fem;
+  typedef Dune::PDELab::NoConstraints CON;
+  typedef Dune::PDELab::ISTLVectorBackend<1> VBE;
+  typedef Dune::PDELab::GridFunctionSpace<GV,FEM,CON,VBE> GFS;
+  GFS gfs(gv,fem);
+
+  // <<<2b>>> Constraints (are empty here)
+  typedef typename GFS::template ConstraintsContainer<Real>::Type C;
+
+  // <<<3>>> Make grid operator space
+  typedef Example01LocalOperator LOP; 
+  LOP lop;
+  typedef Dune::PDELab::ISTLBCRSMatrixBackend<1,1> MBE;
+  typedef Dune::PDELab::GridOperatorSpace<GFS,GFS,LOP,C,C,MBE> GOS;
+  GOS gos(gfs,gfs,lop);
+
+  // <<<4>>> Select a linear solver backend
+#if HAVE_SUPERLU
+  typedef Dune::PDELab::ISTLBackend_SEQ_SuperLU LS;
+  LS ls(true);
+#else
+  typedef Dune::PDELab::ISTLBackend_SEQ_BCGS_SSOR LS;
+  LS ls(5000,true);
+#endif
+
+  // <<<5>>> solve linear problem
+  typedef typename GFS::template VectorContainer<Real>::Type V;
+  V x(gfs,0.0); // initial value
+  Dune::PDELab::StationaryLinearProblemSolver<GOS,LS,V> slp(gos,x,ls,1e-10);
+  slp.apply();
+
+  // <<<6>>> graphical output
+  {
+    typedef Dune::PDELab::DiscreteGridFunction<GFS,V> DGF;
+    DGF xdgf(gfs,x);
+    Dune::VTKWriter<GV> vtkwriter(gv,Dune::VTKOptions::conforming);
+    vtkwriter.addVertexData(new Dune::PDELab::VTKGridFunctionAdapter<DGF>(xdgf,"solution"));
+    vtkwriter.write("example01_sequential_Q1",Dune::VTKOptions::binaryappended);
+  }
+}
+
+// a sequential variant
+template<class GV>
+void example01_sequential_Q2 (const GV& gv)
+{
+  // <<<1>>> Choose domain and range field type
+  typedef typename GV::Grid::ctype Coord;
+  typedef double Real;
+  const int dim = GV::dimension;
+
+  // <<<2a>>> Make grid function space
+  typedef Dune::PDELab::Q22DLocalFiniteElementMap<Coord,Real> FEM;
+  FEM fem;
+  typedef Dune::PDELab::NoConstraints CON;
+  typedef Dune::PDELab::ISTLVectorBackend<1> VBE;
+  typedef Dune::PDELab::GridFunctionSpace<GV,FEM,CON,VBE> GFS;
+  GFS gfs(gv,fem);
+
+  // <<<2b>>> Constraints (are empty here)
+  typedef typename GFS::template ConstraintsContainer<Real>::Type C;
+
+  // <<<3>>> Make grid operator space
+  typedef Example01LocalOperator LOP; 
+  LOP lop(4);
+  typedef Dune::PDELab::ISTLBCRSMatrixBackend<1,1> MBE;
+  typedef Dune::PDELab::GridOperatorSpace<GFS,GFS,LOP,C,C,MBE> GOS;
+  GOS gos(gfs,gfs,lop);
+
+  // <<<4>>> Select a linear solver backend
+#if HAVE_SUPERLU
+  typedef Dune::PDELab::ISTLBackend_SEQ_SuperLU LS;
+  LS ls(true);
+#else
+  typedef Dune::PDELab::ISTLBackend_SEQ_BCGS_SSOR LS;
+  LS ls(5000,true);
+#endif
+
+  // <<<5>>> solve linear problem
+  typedef typename GFS::template VectorContainer<Real>::Type V;
+  V x(gfs,0.0); // initial value
+  Dune::PDELab::StationaryLinearProblemSolver<GOS,LS,V> slp(gos,x,ls,1e-10);
+  slp.apply();
+
+  // <<<6>>> graphical output
+  {
+    typedef Dune::PDELab::DiscreteGridFunction<GFS,V> DGF;
+    DGF xdgf(gfs,x);
+    Dune::SubsamplingVTKWriter<GV> vtkwriter(gv,3);
+    vtkwriter.addVertexData(new Dune::PDELab::VTKGridFunctionAdapter<DGF>(xdgf,"solution"));
+    vtkwriter.write("example01_sequential_Q2",Dune::VTKOptions::binaryappended);
+  }
+}
+
+// a sequential variant
+template<class GV>
+void example01_sequential_RT (const GV& gv)
+{
+  // <<<1>>> Choose domain and range field type
+  typedef typename GV::Grid::ctype Coord;
+  typedef double Real;
+  const int dim = GV::dimension;
+
+  // <<<2a>>> Make grid function space
+  typedef Dune::PDELab::RannacherTurek2DLocalFiniteElementMap<Coord,Real> FEM;
+  FEM fem;
+  typedef Dune::PDELab::NoConstraints CON;
+  typedef Dune::PDELab::ISTLVectorBackend<1> VBE;
+  typedef Dune::PDELab::GridFunctionSpace<GV,FEM,CON,VBE> GFS;
+  GFS gfs(gv,fem);
+
+  // <<<2b>>> Constraints (are empty here)
+  typedef typename GFS::template ConstraintsContainer<Real>::Type C;
+
+  // <<<3>>> Make grid operator space
+  typedef Example01LocalOperator LOP; 
+  LOP lop;
+  typedef Dune::PDELab::ISTLBCRSMatrixBackend<1,1> MBE;
+  typedef Dune::PDELab::GridOperatorSpace<GFS,GFS,LOP,C,C,MBE> GOS;
+  GOS gos(gfs,gfs,lop);
+
+  // <<<4>>> Select a linear solver backend
+#if HAVE_SUPERLU
+  typedef Dune::PDELab::ISTLBackend_SEQ_SuperLU LS;
+  LS ls(true);
+#else
+  typedef Dune::PDELab::ISTLBackend_SEQ_BCGS_SSOR LS;
+  LS ls(5000,true);
+#endif
+
+  // <<<5>>> solve linear problem
+  typedef typename GFS::template VectorContainer<Real>::Type V;
+  V x(gfs,0.0); // initial value
+  Dune::PDELab::StationaryLinearProblemSolver<GOS,LS,V> slp(gos,x,ls,1e-10);
+  slp.apply();
+
+  // <<<6>>> graphical output
+  {
+    typedef Dune::PDELab::DiscreteGridFunction<GFS,V> DGF;
+    DGF xdgf(gfs,x);
+    Dune::SubsamplingVTKWriter<GV> vtkwriter(gv,2);
+    vtkwriter.addVertexData(new Dune::PDELab::VTKGridFunctionAdapter<DGF>(xdgf,"solution"));
+    vtkwriter.write("example01_sequential_RT",Dune::VTKOptions::binaryappended);
+  }
+}
+
+// a sequential variant
+template<class GV>
+void example01b_sequential_Q2 (const GV& gv)
+{
+  // <<<1>>> Choose domain and range field type
+  typedef typename GV::Grid::ctype Coord;
+  typedef double Real;
+  const int dim = GV::dimension;
+
+  // <<<2a>>> Make grid function space
+  typedef Dune::PDELab::Q22DLocalFiniteElementMap<Coord,Real> FEM;
+  FEM fem;
+  typedef Dune::PDELab::NoConstraints CON;
+  typedef Dune::PDELab::ISTLVectorBackend<1> VBE;
+  typedef Dune::PDELab::GridFunctionSpace<GV,FEM,CON,VBE> GFS;
+  GFS gfs(gv,fem);
+
+  // <<<2b>>> Constraints (are empty here)
+  typedef typename GFS::template ConstraintsContainer<Real>::Type C;
+
+  // <<<3>>> Make grid operator space
+  typedef Example01LocalOperator LOP; 
+  LOP lop(4);
+  typedef Dune::PDELab::ISTLBCRSMatrixBackend<1,1> MBE;
+  typedef Dune::PDELab::GridOperatorSpace<GFS,GFS,LOP,C,C,MBE> GOS;
+  GOS gos(gfs,gfs,lop);
+
+  // <<<4>>> Select a linear solver backend
+#if HAVE_SUPERLU
+  typedef Dune::PDELab::ISTLBackend_SEQ_SuperLU LS;
+  LS ls(true);
+#else
+  typedef Dune::PDELab::ISTLBackend_SEQ_BCGS_SSOR LS;
+  LS ls(5000,true);
+#endif
+
+  // <<<5>>> solve nonlinear problem
+  typedef typename GFS::template VectorContainer<Real>::Type V;
+  V x(gfs,2.0); // initial value
+  Dune::PDELab::Newton<GOS,LS,V> newton(gos,x,ls);
+  newton.setReassembleThreshold(0.0);
+  newton.setVerbosityLevel(2);
+  newton.setReduction(1e-10);
+  newton.setMinLinearReduction(1e-4);
+  newton.setMaxIterations(25);
+  newton.setLineSearchMaxIterations(10);
+  newton.apply();
+
+  // <<<6>>> graphical output
+  {
+    typedef Dune::PDELab::DiscreteGridFunction<GFS,V> DGF;
+    DGF xdgf(gfs,x);
+    Dune::SubsamplingVTKWriter<GV> vtkwriter(gv,3);
+    vtkwriter.addVertexData(new Dune::PDELab::VTKGridFunctionAdapter<DGF>(xdgf,"solution"));
+    vtkwriter.write("example01b_sequential_Q2",Dune::VTKOptions::binaryappended);
+  }
+}
+
+//===============================================================
+// Main program with grid setup
+//===============================================================
+
+int main(int argc, char** argv)
+{
+  try{
+    //Maybe initialize Mpi
+    Dune::MPIHelper& helper = Dune::MPIHelper::instance(argc, argv);
+    if(Dune::MPIHelper::isFake)
+      std::cout<< "This is a sequential program." << std::endl;
+    else
+	  {
+		if(helper.rank()==0)
+		  std::cout << "parallel run on " << helper.size() << " process(es)" << std::endl;
+	  }
+
+	if (argc!=2)
+	  {
+		if(helper.rank()==0)
+		  std::cout << "usage: ./example01 <level>" << std::endl;
+		return 1;
+	  }
+
+	int level;
+	sscanf(argv[1],"%d",&level);
+
+    // sequential version
+    if (1 && helper.size()==1)
+    {
+      Dune::FieldVector<double,2> L(1.0);
+      Dune::FieldVector<int,2> N(1);
+      Dune::FieldVector<bool,2> periodic(false);
+      int overlap=0;
+      Dune::YaspGrid<2> grid(L,N,periodic,overlap);
+      grid.globalRefine(level);
+      typedef Dune::YaspGrid<2>::LeafGridView GV;
+      const GV& gv=grid.leafView();
+      example01_sequential_Q1(gv);
+      example01_sequential_Q2(gv);
+      example01_sequential_RT(gv);
+      example01b_sequential_Q2(gv);
+    }
+  }
+  catch (Dune::Exception &e){
+    std::cerr << "Dune reported error: " << e << std::endl;
+	return 1;
+  }
+  catch (...){
+    std::cerr << "Unknown exception thrown!" << std::endl;
+	return 1;
+  }
+}
