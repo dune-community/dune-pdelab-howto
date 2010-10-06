ebff3daa19ed995e92ecd3cd7940c163a237abb0
Markus Blatt <mblatt@dune-project.org>
2010-10-06 12:21:16 +0000

    IF Superlu is not present, skip the flags for the executables not being compiled
    Imported from SVN: r248 at trunk

--- examples/dnaplfv.cc
+++ examples/dnaplfv.cc
@@ -505,12 +505,12 @@ void test (const GV& gv, int timesteps, double timestep)
   IGOS igos(method,tpgfs,cg,tpgfs,cg,lop,mlop);
 
   // <<<10>>> Make a linear solver 
-  typedef Dune::PDELab::ISTLBackend_OVLP_BCGS_SSORk<TPGFS,C> LS;
-  LS ls(tpgfs,cg,5000,5,1);
+  //typedef Dune::PDELab::ISTLBackend_OVLP_BCGS_SSORk<TPGFS,C> LS;
+  //LS ls(tpgfs,cg,5000,5,1);
   
   // Comment out above and uncomment to use Parallel AMG
-  //typedef  Dune::PDELab::ISTLBackend_BCGS_AMG_SSOR<TPGFS> LS;
-  //LS ls (tpgfs,1);
+  typedef  Dune::PDELab::ISTLBackend_BCGS_AMG_SSOR<TPGFS> LS;
+  LS ls (tpgfs,1, 2000, 2);
 
   // <<<11>>> make Newton for time-dependent problem
   typedef Dune::PDELab::Newton<IGOS,LS,V> PDESOLVER;
