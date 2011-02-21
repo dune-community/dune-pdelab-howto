21ef0f250aebcd441270bf76f42f579b7b1ad922
Peter Bastian <peter@dune-project.org>
2011-02-21 21:07:25 +0000

    moved files
    Imported from SVN: r279 at trunk

--- examples/q1localfiniteelementmap.hh
+++
@@ -1,8 +0,0 @@
-#include<dune/pdelab/finiteelementmap/finiteelementmap.hh>
-#include"q1localfiniteelement.hh"
-
-template<class D, class R>
-class Q1LocalFiniteElementMap
-  : public Dune::PDELab::
-      SimpleLocalFiniteElementMap< Q1LocalFiniteElement<D,R> >
-{};
