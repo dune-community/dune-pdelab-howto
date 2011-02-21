4122a2da1f886119746eb5556b9bb86519a33421
Peter Bastian <peter@dune-project.org>
2011-02-21 20:17:25 +0000

    examples are running
    Imported from SVN: r273 at trunk

---
+++ src/course/example01.cc
@@ -0,0 +1,76 @@
+// -*- tab-width: 4; indent-tabs-mode: nil -*-
+/** \file
+    
+    \brief Solve elliptic problem in unconstrained spaces with conforming finite elements
+*/
+#ifdef HAVE_CONFIG_H
+#include "config.h"     
+#endif
+#include<math.h>
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+
+#include<dune/common/mpihelper.hh>
+#include<dune/common/exceptions.hh>
+#include<dune/common/fvector.hh>
+#include<dune/common/static_assert.hh>
+#include<dune/common/timer.hh>
+#include<dune/grid/io/file/vtk/subsamplingvtkwriter.hh>
+#include<dune/grid/io/file/gmshreader.hh>
+#include<dune/grid/yaspgrid.hh>
+#if HAVE_ALBERTA
+#include<dune/grid/albertagrid.hh>
+#include <dune/grid/albertagrid/dgfparser.hh>
+#endif
+#if HAVE_UG 
+#include<dune/grid/uggrid.hh>
+#endif
+#if HAVE_ALUGRID
+#include<dune/grid/alugrid.hh>
+#include<dune/grid/io/file/dgfparser/dgfalu.hh>
+#include<dune/grid/io/file/dgfparser/dgfparser.hh>
+#endif
+#include<dune/istl/bvector.hh>
+#include<dune/istl/operators.hh>
+#include<dune/istl/solvers.hh>
+#include<dune/istl/preconditioners.hh>
+#include<dune/istl/io.hh>
+#include<dune/istl/superlu.hh>
+
+#include<dune/pdelab/finiteelementmap/p0fem.hh>
+#include<dune/pdelab/finiteelementmap/p12dfem.hh>
+#include<dune/pdelab/finiteelementmap/pk2dfem.hh>
+#include<dune/pdelab/finiteelementmap/pk3dfem.hh>
+#include<dune/pdelab/finiteelementmap/q12dfem.hh>
+#include<dune/pdelab/finiteelementmap/q22dfem.hh>
+#include<dune/pdelab/finiteelementmap/q1fem.hh>
+#include<dune/pdelab/finiteelementmap/p1fem.hh>
+#include<dune/pdelab/finiteelementmap/rannacher_turek2dfem.hh>
+#include<dune/pdelab/finiteelementmap/conformingconstraints.hh>
+#include<dune/pdelab/gridfunctionspace/gridfunctionspace.hh>
+#include<dune/pdelab/gridfunctionspace/gridfunctionspaceutilities.hh>
+#include<dune/pdelab/gridfunctionspace/genericdatahandle.hh>
+#include<dune/pdelab/gridfunctionspace/interpolate.hh>
+#include<dune/pdelab/gridfunctionspace/constraints.hh>
+#include<dune/pdelab/common/function.hh>
+#include<dune/pdelab/common/vtkexport.hh>
+#include<dune/pdelab/gridoperatorspace/gridoperatorspace.hh>
+#include<dune/pdelab/backend/istlvectorbackend.hh>
+#include<dune/pdelab/backend/istlmatrixbackend.hh>
+#include<dune/pdelab/backend/istlsolverbackend.hh>
+#include<dune/pdelab/stationary/linearproblem.hh>
+
+#include"example01a_operator.hh"
+#include"example01b_operator.hh"
+#include"example01a_Q1.hh"
+#include"example01a_Q2.hh"
+#include"example01a_RT.hh"
+#include"example01b_Q2.hh"
+
+//===============================================================
+// Main program with grid setup
+//===============================================================
+
+#include"example01_main.hh"
---
+++ src/course/example06.cc
@@ -0,0 +1,133 @@
+// -*- tab-width: 4; indent-tabs-mode: nil -*-
+/** \file
+    
+    \brief Solve two-component diffusion-reaction system with conforming finite elements in parallel on overlapping grids
+*/
+#ifdef HAVE_CONFIG_H
+#include "config.h"     
+#endif
+#include<math.h>
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<stdlib.h>
+
+#include<dune/common/mpihelper.hh>
+#include<dune/common/exceptions.hh>
+#include<dune/common/fvector.hh>
+#include<dune/common/static_assert.hh>
+#include<dune/common/timer.hh>
+#include<dune/grid/io/file/vtk/subsamplingvtkwriter.hh>
+#include<dune/grid/io/file/gmshreader.hh>
+#include<dune/grid/yaspgrid.hh>
+#if HAVE_ALBERTA
+#include<dune/grid/albertagrid.hh>
+#include <dune/grid/albertagrid/dgfparser.hh>
+#endif
+#if HAVE_UG 
+#include<dune/grid/uggrid.hh>
+#endif
+#if HAVE_ALUGRID
+#include<dune/grid/alugrid.hh>
+#include<dune/grid/io/file/dgfparser/dgfalu.hh>
+#include<dune/grid/io/file/dgfparser/dgfparser.hh>
+#endif
+#include<dune/istl/bvector.hh>
+#include<dune/istl/operators.hh>
+#include<dune/istl/solvers.hh>
+#include<dune/istl/preconditioners.hh>
+#include<dune/istl/io.hh>
+#include<dune/istl/superlu.hh>
+
+#include<dune/pdelab/common/function.hh>
+#include<dune/pdelab/common/vtkexport.hh>
+#include<dune/pdelab/finiteelementmap/p0fem.hh>
+#include<dune/pdelab/finiteelementmap/p12dfem.hh>
+#include<dune/pdelab/finiteelementmap/pk2dfem.hh>
+#include<dune/pdelab/finiteelementmap/pk3dfem.hh>
+#include<dune/pdelab/finiteelementmap/q12dfem.hh>
+#include<dune/pdelab/finiteelementmap/q22dfem.hh>
+#include<dune/pdelab/finiteelementmap/q1fem.hh>
+#include<dune/pdelab/finiteelementmap/p1fem.hh>
+#include<dune/pdelab/finiteelementmap/rannacher_turek2dfem.hh>
+#include<dune/pdelab/finiteelementmap/conformingconstraints.hh>
+#include<dune/pdelab/gridfunctionspace/gridfunctionspace.hh>
+#include<dune/pdelab/gridfunctionspace/gridfunctionspaceutilities.hh>
+#include<dune/pdelab/gridfunctionspace/genericdatahandle.hh>
+#include<dune/pdelab/gridfunctionspace/interpolate.hh>
+#include<dune/pdelab/gridfunctionspace/constraints.hh>
+#include<dune/pdelab/gridoperatorspace/gridoperatorspace.hh>
+#include<dune/pdelab/gridoperatorspace/instationarygridoperatorspace.hh>
+#include<dune/pdelab/backend/istlvectorbackend.hh>
+#include<dune/pdelab/backend/istlmatrixbackend.hh>
+#include<dune/pdelab/backend/istlsolverbackend.hh>
+#include<dune/pdelab/stationary/linearproblem.hh>
+#include<dune/pdelab/instationary/onestep.hh>
+
+#include"example05_operator.hh"
+#include"example05_toperator.hh"
+#include"example05_initial.hh"
+#include"example06_bctype.hh"
+#include"example06_Q1Q1.hh"
+
+//===============================================================
+// Main program with grid setup
+//===============================================================
+
+int main(int argc, char** argv)
+{
+  try{
+    //Maybe initialize Mpi
+    Dune::MPIHelper& helper = Dune::MPIHelper::instance(argc, argv);
+    if(Dune::MPIHelper::isFake)
+      std::cout<< "This is a sequential program." << std::endl;
+    else
+	  {
+		if(helper.rank()==0)
+		  std::cout << "parallel run on " << helper.size() << " process(es)" << std::endl;
+	  }
+
+	if (argc!=5)
+	  {
+		if(helper.rank()==0)
+		  std::cout << "usage: ./example06 <level> <dtstart> <dtmax> <tend>" << std::endl;
+		return 1;
+	  }
+
+	int level;
+	sscanf(argv[1],"%d",&level);
+
+	double dtstart;
+	sscanf(argv[2],"%lg",&dtstart);
+
+	double dtmax;
+	sscanf(argv[3],"%lg",&dtmax);
+
+	double tend;
+	sscanf(argv[4],"%lg",&tend);
+
+#if HAVE_MPI
+    // 2D
+    {
+      // make grid
+      Dune::FieldVector<double,2> L(2.0);
+      Dune::FieldVector<int,2> N(1<<level);
+      Dune::FieldVector<bool,2> B(false);
+      int overlap=3;
+      Dune::YaspGrid<2> grid(helper.getCommunicator(),L,N,B,overlap);
+      typedef Dune::YaspGrid<2>::LeafGridView GV;
+      const GV& gv=grid.leafView();
+      example06_Q1Q1(gv,dtstart,dtmax,tend);
+    }
+#endif
+  }
+  catch (Dune::Exception &e){
+    std::cerr << "Dune reported error: " << e << std::endl;
+	return 1;
+  }
+  catch (...){
+    std::cerr << "Unknown exception thrown!" << std::endl;
+	return 1;
+  }
+}
