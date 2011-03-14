db7b47fd69a64622ecacdcb6c98853c517b9771c
Peter Bastian <peter@dune-project.org>
2011-03-14 16:10:47 +0000

    do local refinement with new indicator
    Imported from SVN: r384 at trunk

--- src/convection-diffusion/ldomain.cc
+++ src/convection-diffusion/ldomain.cc
@@ -25,13 +25,15 @@
 #include <dune/pdelab/gridfunctionspace/interpolate.hh>
 #include <dune/pdelab/gridfunctionspace/constraints.hh>
 #include <dune/pdelab/gridoperatorspace/gridoperatorspace.hh>
-#include<dune/pdelab/finiteelementmap/pkfem.hh>
 #include<dune/pdelab/finiteelementmap/p0fem.hh>
+#include<dune/pdelab/finiteelementmap/p1fem.hh>
 #include<dune/pdelab/finiteelementmap/conformingconstraints.hh>
 #include<dune/pdelab/localoperator/convectiondiffusionparameter.hh>
 #include<dune/pdelab/localoperator/convectiondiffusionfem.hh>
 #include<dune/pdelab/stationary/linearproblem.hh>
+#include<dune/pdelab/adaptivity/adapt.hh>
 
+#include"../utility/gridexamples.hh"
 
 template<typename GV, typename RF>
 class ReentrantCornerProblem
@@ -234,99 +236,171 @@ private:
   const T2& t2;
 };
 
-//! solve the diffusion problem on a given GridView with a given finite element space
-template<class GV, class FEM, class CON>
-void driver (const GV& gv, FEM& fem, std::string filename_base, int& N, double& l2e, double& h1se, double& ee)
+//! Solve problem on leaf grid view and adapt grid
+template<class Grid>
+void driver (Grid& grid, std::string filename_base, double TOL, int maxsteps, double fraction)
 {
-  // coordinate and result type
-  typedef typename GV::Grid::ctype Coord;
+  // some types
+  typedef typename Grid::LeafGridView GV;
+  typedef typename Grid::ctype Coord;
   typedef double Real;
   const int dim = GV::dimension;
 
+  // some arrays to store results
+  std::vector<double> l2;
+  std::vector<double> h1s;
+  std::vector<double> ee;
+  std::vector<int> N;
+
+  // make finite element map
+  // note: adaptivity currently relies on finite element map not depending on grid view
+  typedef Dune::PDELab::P1LocalFiniteElementMap<Coord,Real,dim> FEM;
+  FEM fem;
+
   // make grid function space 
+  // note: adaptivity relies on leaf grid view object being updated by the grid on adaptation 
+  typedef Dune::PDELab::ConformingDirichletConstraints CON;
   typedef Dune::PDELab::ISTLVectorBackend<1> VBE;
   typedef Dune::PDELab::GridFunctionSpace<GV,FEM,CON,VBE> GFS;
-  GFS gfs(gv,fem);
-  N = gfs.globalSize();
+  GFS gfs(grid.leafView(),fem);
+  N.push_back(gfs.globalSize());
 
-  // make problem
-  typedef ReentrantCornerProblem<GV,double> Problem;
-  Problem problem;
-
-  // make a std::vector of degree of freedom vectors and initialize it with Dirichlet extension
+  // make a degree of freedom vector;
   typedef typename Dune::PDELab::BackendVectorSelector<GFS,Real>::Type U;
   U u(gfs,0.0);
-  typedef Dune::PDELab::ConvectionDiffusionDirichletExtensionAdapter<Problem> G;
-  G g(gv,problem);
-  Dune::PDELab::interpolate(g,gfs,u);
 
-  // make constraints container and initialize it
-  typedef typename GFS::template ConstraintsContainer<Real>::Type CC;
-  CC cc;
-  Dune::PDELab::ConvectionDiffusionBoundaryConditionAdapter<Problem> bctype(gv,problem);
-  Dune::PDELab::constraints(bctype,gfs,cc);
-  Dune::PDELab::set_nonconstrained_dofs(cc,0.0,u);
-  std::cout << "constrained dofs=" << cc.size() << " of " << gfs.globalSize() << std::endl;
+  // refinement loop
+  for (int step=0; step<maxsteps; step++)
+    {
+      // get current leaf view
+      const GV& gv=grid.leafView();
 
-  // make local operator
-  typedef Dune::PDELab::ConvectionDiffusionFEM<Problem,FEM> LOP;
-  LOP lop(problem);
-  typedef Dune::PDELab::ISTLBCRSMatrixBackend<1,1> MBE;
-  typedef Dune::PDELab::GridOperatorSpace<GFS,GFS,LOP,CC,CC,MBE> GOS;
-  GOS gos(gfs,cc,gfs,cc,lop);
+      // make problem
+      typedef ReentrantCornerProblem<GV,Real> Problem;
+      Problem problem;
 
-  // make linear solver and solve problem
-  typedef Dune::PDELab::ISTLBackend_SEQ_CG_AMG_SSOR<GFS> LS;
-  LS ls (2, 5000, 1);
-  // typedef Dune::PDELab::ISTLBackend_SEQ_CG_ILU0 LS;
-  // LS ls(10000,1);
-  typedef Dune::PDELab::StationaryLinearProblemSolver<GOS,LS,U> SLP;
-  SLP slp(gos,u,ls,1e-10);
-  slp.apply();
+      // initialize DOFS from Dirichlet extension
+      // note: currently we start from scratch on every grid
+      typedef Dune::PDELab::ConvectionDiffusionDirichletExtensionAdapter<Problem> G;
+      G g(gv,problem);
+      Dune::PDELab::interpolate(g,gfs,u);
 
-  // compute errors
-  typedef Dune::PDELab::DiscreteGridFunction<GFS,U> DGF;
-  DGF udgf(gfs,u);
-  typedef DifferenceSquaredAdapter<G,DGF> DifferenceSquared;
-  DifferenceSquared differencesquared(g,udgf);
-  typename DifferenceSquared::Traits::RangeType l2errorsquared(0.0);
-  Dune::PDELab::integrateGridFunction(differencesquared,l2errorsquared,10);
-  l2e = sqrt(l2errorsquared);
-  typedef Dune::PDELab::DiscreteGridFunctionGradient<GFS,U> DGFGrad;
-  DGFGrad udgfgrad(gfs,u);
-  typedef ExactGradient<GV,double> Grad;
-  Grad grad(gv);
-  typedef DifferenceSquaredAdapter<Grad,DGFGrad> GradDifferenceSquared;
-  GradDifferenceSquared graddifferencesquared(grad,udgfgrad);
-  typename GradDifferenceSquared::Traits::RangeType h1semierrorsquared(0.0);
-  Dune::PDELab::integrateGridFunction(graddifferencesquared,h1semierrorsquared,10);
-  h1se = sqrt(h1semierrorsquared);
+      // make constraints container and initialize it
+      typedef typename GFS::template ConstraintsContainer<Real>::Type CC;
+      CC cc;
+      Dune::PDELab::ConvectionDiffusionBoundaryConditionAdapter<Problem> bctype(gv,problem);
+      Dune::PDELab::constraints(bctype,gfs,cc);
+      Dune::PDELab::set_nonconstrained_dofs(cc,0.0,u);
+      std::cout << "constrained dofs=" << cc.size() << " of " << gfs.globalSize() << std::endl;
 
-  // compute estimated error
-  typedef Dune::PDELab::P0LocalFiniteElementMap<Coord,Real,dim> P0FEM;
-  P0FEM p0fem(Dune::GeometryType(Dune::GeometryType::simplex,dim));
-  typedef Dune::PDELab::GridFunctionSpace<GV,P0FEM,Dune::PDELab::NoConstraints,VBE> P0GFS; 
-  P0GFS p0gfs(gv,p0fem);
+      // make local operator
+      typedef Dune::PDELab::ConvectionDiffusionFEM<Problem,FEM> LOP;
+      LOP lop(problem);
+      typedef Dune::PDELab::ISTLBCRSMatrixBackend<1,1> MBE;
+      typedef Dune::PDELab::GridOperatorSpace<GFS,GFS,LOP,CC,CC,MBE> GOS;
+      GOS gos(gfs,cc,gfs,cc,lop);
 
-  typedef Dune::PDELab::ConvectionDiffusionFEMResidualEstimator<Problem> ESTLOP;
-  ESTLOP estlop(problem);
-  typedef Dune::PDELab::GridOperatorSpace<GFS,P0GFS,ESTLOP,Dune::PDELab::EmptyTransformation,Dune::PDELab::EmptyTransformation,MBE> ESTGOS;
-  ESTGOS estgos(gfs,p0gfs,estlop);
+      // make linear solver and solve problem
+      typedef Dune::PDELab::ISTLBackend_SEQ_CG_AMG_SSOR<GFS> LS;
+      LS ls (2, 5000, 1);
+      // typedef Dune::PDELab::ISTLBackend_SEQ_CG_ILU0 LS;
+      // LS ls(10000,1);
+      typedef Dune::PDELab::StationaryLinearProblemSolver<GOS,LS,U> SLP;
+      SLP slp(gos,u,ls,1e-10);
+      slp.apply();
 
-  typedef typename Dune::PDELab::BackendVectorSelector<P0GFS,Real>::Type U0;
-  U0 eta(p0gfs,0.0);
-  estgos.residual(u,eta);
-  ee = sqrt(eta.one_norm());
+      // compute errors
+      typedef Dune::PDELab::DiscreteGridFunction<GFS,U> DGF;
+      DGF udgf(gfs,u);
+      typedef DifferenceSquaredAdapter<G,DGF> DifferenceSquared;
+      DifferenceSquared differencesquared(g,udgf);
+      typename DifferenceSquared::Traits::RangeType l2errorsquared(0.0);
+      Dune::PDELab::integrateGridFunction(differencesquared,l2errorsquared,10);
+      l2.push_back(sqrt(l2errorsquared));
+      typedef Dune::PDELab::DiscreteGridFunctionGradient<GFS,U> DGFGrad;
+      DGFGrad udgfgrad(gfs,u);
+      typedef ExactGradient<GV,Real> Grad;
+      Grad grad(gv);
+      typedef DifferenceSquaredAdapter<Grad,DGFGrad> GradDifferenceSquared;
+      GradDifferenceSquared graddifferencesquared(grad,udgfgrad);
+      typename GradDifferenceSquared::Traits::RangeType h1semierrorsquared(0.0);
+      Dune::PDELab::integrateGridFunction(graddifferencesquared,h1semierrorsquared,10);
+      h1s.push_back(sqrt(h1semierrorsquared));
+      
+      // compute estimated error
+      typedef Dune::PDELab::P0LocalFiniteElementMap<Coord,Real,dim> P0FEM;
+      P0FEM p0fem(Dune::GeometryType(Dune::GeometryType::simplex,dim));
+      typedef Dune::PDELab::GridFunctionSpace<GV,P0FEM,Dune::PDELab::NoConstraints,VBE> P0GFS; 
+      P0GFS p0gfs(gv,p0fem);
+      typedef Dune::PDELab::ConvectionDiffusionFEMResidualEstimator<Problem> ESTLOP;
+      ESTLOP estlop(problem);
+      typedef Dune::PDELab::EmptyTransformation NoTrafo;
+      typedef Dune::PDELab::GridOperatorSpace<GFS,P0GFS,ESTLOP,NoTrafo,NoTrafo,MBE> ESTGOS;
+      ESTGOS estgos(gfs,p0gfs,estlop);
+      typedef typename Dune::PDELab::BackendVectorSelector<P0GFS,Real>::Type U0;
+      U0 eta(p0gfs,0.0);
+      estgos.residual(u,eta);
+      for (int i=0; i<eta.N(); i++) eta[i] = sqrt(eta[i]); 
+      Real estimated_error = eta.two_norm();
+      ee.push_back(estimated_error);
 
-  // write vtk file
-  // typedef DifferenceAdapter<G,DGF> Difference;
-  // Difference difference(g,udgf);
-  // Dune::SubsamplingVTKWriter<GV> vtkwriter(gv,0);
-  // //Dune::VTKWriter<GV> vtkwriter(gv);
-  // vtkwriter.addVertexData(new Dune::PDELab::VTKGridFunctionAdapter<DGF>(udgf,"u_h"));
-  // vtkwriter.addVertexData(new Dune::PDELab::VTKGridFunctionAdapter<G>(g,"u"));
-  // vtkwriter.addVertexData(new Dune::PDELab::VTKGridFunctionAdapter<Difference>(difference,"u-u_h"));
-  // vtkwriter.write(filename_base,Dune::VTKOptions::binaryappended);
+      // write vtk file
+      typedef Dune::PDELab::DiscreteGridFunction<P0GFS,U0> DGF0;
+      DGF0 udgf0(p0gfs,eta);
+      typedef DifferenceAdapter<G,DGF> Difference;
+      Difference difference(g,udgf);
+      //Dune::SubsamplingVTKWriter<GV> vtkwriter(gv,2);
+      Dune::VTKWriter<GV> vtkwriter(gv);
+      std::stringstream fullname;
+      fullname << filename_base << "_step" << step;
+      vtkwriter.addVertexData(new Dune::PDELab::VTKGridFunctionAdapter<DGF>(udgf,"u_h"));
+      vtkwriter.addVertexData(new Dune::PDELab::VTKGridFunctionAdapter<G>(g,"u"));
+      vtkwriter.addCellData(new Dune::PDELab::VTKGridFunctionAdapter<Difference>(difference,"u-u_h"));
+      vtkwriter.addCellData(new Dune::PDELab::VTKGridFunctionAdapter<DGF0>(udgf0,"estimated error"));
+      vtkwriter.write(fullname.str(),Dune::VTKOptions::binaryappended);
+
+      // error control
+      if (estimated_error <= TOL) break;
+
+      // create grid adaption classes
+      typedef Dune::PDELab::L2Projection<GFS,U> Proj;
+      Proj proj;
+      typedef Dune::PDELab::ResidualErrorEstimation<GFS,U,ESTLOP> REE;
+      REE ree(gfs,estlop);
+      typedef Dune::PDELab::EstimationAdaptation<Grid,GFS,U,REE> EA;
+      EA ea(grid,gfs,ree,fraction,0.0);
+      typedef Dune::PDELab::GridAdaptor<Grid,GFS,U,EA,Proj> GRA;
+      GRA gra(grid,gfs,ea,proj);
+      
+      // adapt grid
+      if (step<maxsteps-1) 
+	{
+	  gra.adapt(u);
+	  N.push_back(gfs.globalSize());
+	}
+    }
+
+  // print results
+  std::cout << "Results on mesh=" << filename_base << std::endl; 
+  std::cout << "N l2 l2rate h1semi h1semirate estimator effectivity" << std::endl;
+  for (std::size_t i=0; i<N.size(); i++) 
+    {
+      double rate1=0.0;
+      if (i>0) rate1=log(l2[i]/l2[i-1])/log(0.5);
+      double rate2=0.0;
+      if (i>0) rate2=log(h1s[i]/h1s[i-1])/log(0.5); 
+      std::cout << std::setw(10) << i
+		<< std::setw(10) << N[i]
+		<< std::setw(12) << std::setprecision(4) << std::scientific << l2[i]
+		<< std::setw(12) << std::setprecision(4) << std::scientific << rate1
+		<< std::setw(12) << std::setprecision(4) << std::scientific << h1s[i]
+		<< std::setw(12) << std::setprecision(4) << std::scientific << rate2
+		<< std::setw(12) << std::setprecision(4) << std::scientific << ee[i]
+		<< std::setw(12) << std::setprecision(4) << std::scientific << ee[i]/(h1s[i])
+		<< std::endl;
+    }
+  
+
 }
 
 int main(int argc, char **argv)
@@ -342,61 +416,36 @@ int main(int argc, char **argv)
     if (true)
     {
       // read command line arguments
-      if (argc!=3)
+      if (argc!=6)
 	{
-	  std::cout << "usage: " << argv[0] << " <mesh file> <maxlevel>" << std::endl;
+	  std::cout << "usage: " << argv[0] << " <mesh file> <start level> <TOL> <max steps> <fraction>" << std::endl;
 	  return 0;
 	}
       std::string grid_file(argv[1]);
-      int max_level; sscanf(argv[2],"%d",&max_level);
+      int start_level; sscanf(argv[2],"%d",&start_level);
+      double TOL; sscanf(argv[3],"%lg",&TOL);
+      int maxsteps; sscanf(argv[4],"%d",&maxsteps);
+      double fraction; sscanf(argv[5],"%lg",&fraction);
 
       // make uggrid
       const int dim=2;
-      typedef Dune::UGGrid<dim> GridType;
-      GridType grid;
-      typedef std::vector<int> GmshIndexMap;
-      GmshIndexMap boundary_index_map;
-      GmshIndexMap element_index_map;
-      Dune::GmshReader<GridType> gmsh_reader;
-      gmsh_reader.read(grid,grid_file,boundary_index_map,element_index_map,true,false);
-      
-      std::vector<double> l2(max_level+1,0.0);
-      std::vector<double> h1s(max_level+1,0.0);
-      std::vector<double> ee(max_level+1,0.0);
-      std::vector<int> N(max_level+1,0);
 
-      for (int i=0; i<=max_level; i++)
-	{
-	  typedef GridType::LeafGridView GV;
-	  const GV& gv=grid.leafView();
+      // make Alberta grid
+      typedef AlbertaLDomain::Grid GridType;
+      AlbertaLDomain gridp;
+      GridType &grid = gridp;
 
-	  // make finite element space and solve
-	  typedef Dune::PDELab::ConformingDirichletConstraints CON;
-	  typedef Dune::PDELab::PkLocalFiniteElementMap<GV,GridType::ctype,double,1,dim> FEM;
-	  FEM fem(gv);
-	  std::stringstream filename;
-	  filename << "ldomain_" << grid_file << "_l" << i;
-	  driver<GV,FEM,CON>(gv,fem,filename.str(),N[i],l2[i],h1s[i],ee[i]);
-	  if (i<max_level) grid.globalRefine(1);
-	}
+      // make UG grid
+      // typedef Dune::UGGrid<dim> GridType;
+      // GridType grid;
+      // typedef std::vector<int> GmshIndexMap;
+      // GmshIndexMap boundary_index_map;
+      // GmshIndexMap element_index_map;
+      // Dune::GmshReader<GridType> gmsh_reader;
+      // gmsh_reader.read(grid,grid_file,boundary_index_map,element_index_map,true,false);
 
-    std::cout << "Results on mesh=" << grid_file << std::endl; 
-    std::cout << "N l2 l2rate h1semi h1semirate estimator effectivity" << std::endl;
-    for (int i=0; i<=max_level; i++) 
-      {
-	double rate1=0.0;
-	if (i>0) rate1=log(l2[i]/l2[i-1])/log(0.5);
-	double rate2=0.0;
-	if (i>0) rate2=log(h1s[i]/h1s[i-1])/log(0.5); 
-	std::cout << std::setw(10) << N[i]
-		  << std::setw(12) << std::setprecision(4) << std::scientific << l2[i]
-		  << std::setw(12) << std::setprecision(4) << std::scientific << rate1
-		  << std::setw(12) << std::setprecision(4) << std::scientific << h1s[i]
-		  << std::setw(12) << std::setprecision(4) << std::scientific << rate2
-		  << std::setw(12) << std::setprecision(4) << std::scientific << ee[i]
-		  << std::setw(12) << std::setprecision(4) << std::scientific << ee[i]/(h1s[i])
-		  << std::endl;
-	}
+      for (int i=0; i<start_level; i++) grid.globalRefine(1);
+      driver(grid,"ldomain",TOL,maxsteps,fraction);
     }
 #endif
   }
