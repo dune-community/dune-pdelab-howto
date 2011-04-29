9cf477eeb296880ab1f963d71527527d0d8ec73a
Rebecca Neumann <rebecca@dune-project.org>
2011-04-29 12:56:04 +0000

    adapt to AMG interface change: steps is not a constructor argument anymore
    Imported from SVN: r442 at trunk

--- src/convection-diffusion/overlappingsinglephaseflowccfv.cc
+++ src/convection-diffusion/overlappingsinglephaseflowccfv.cc
@@ -107,7 +107,7 @@ void test (const GV& gv)
   Dune::PDELab::interpolate(g,gfs,x);
 
   typedef  Dune::PDELab::ISTLBackend_BCGS_AMG_SSOR<GO> LS;
-  LS ls(gfs,1,5000,3);
+  LS ls(gfs,5000,3);
   typedef Dune::PDELab::StationaryLinearProblemSolver<GO,LS,V> SLP;
   SLP slp(go,x,ls,1e-12);
   slp.apply();
