d0293ceecef8ea0bb80131ed2071aa2ee15181c7
Peter Bastian <peter@dune-project.org>
2011-08-03 08:56:11 +0000

    check in a solver that compiles
    Imported from SVN: r476 at trunk

--- src/convection-diffusion/overlappingsinglephaseflowccfv.cc
+++ src/convection-diffusion/overlappingsinglephaseflowccfv.cc
@@ -106,8 +106,10 @@ void test (const GV& gv)
   x = 0.0;
   Dune::PDELab::interpolate(g,gfs,x);
 
-  typedef  Dune::PDELab::ISTLBackend_BCGS_AMG_SSOR<GO> LS;
-  LS ls(gfs,5000,3);
+  // typedef  Dune::PDELab::ISTLBackend_BCGS_AMG_SSOR<GO> LS;
+  // LS ls(gfs,5000,3);
+  typedef Dune::PDELab::ISTLBackend_OVLP_BCGS_SSORk<GFS,CC> LS;
+  LS ls(gfs,cc,5000,5,1);
   typedef Dune::PDELab::StationaryLinearProblemSolver<GO,LS,V> SLP;
   SLP slp(go,x,ls,1e-12);
   slp.apply();
