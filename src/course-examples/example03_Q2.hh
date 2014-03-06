b5a69fbe1d0f7b5ddfb64940801af6eb6dbf3c9d
Peter Bastian <peter.bastian@iwr.uni-heidelberg.de>
2014-03-06 09:39:47 +0100

    changed indentation of comments

--- src/course-examples/example03_Q2.hh
+++ src/course-examples/example03_Q2.hh
@@ -4,7 +4,7 @@ void example03_Q2 (const GV& gv, double dt, double tend)
   // <<<1>>> Choose domain and range field type
   typedef typename GV::Grid::ctype Coord;
   typedef double Real;
-  Real time = 0.0;                                              // make a time variable
+  Real time = 0.0;                                           // make a time variable
 
   // <<<2>>> Make grid function space
   typedef Dune::PDELab::QkLocalFiniteElementMap<GV,Coord,Real,2> FEM;
@@ -16,16 +16,16 @@ void example03_Q2 (const GV& gv, double dt, double tend)
   gfs.name("solution");
 
   BCTypeParam bctype;
-  bctype.setTime(time);                                              // b.c. depends on time now
+  bctype.setTime(time);                                      // b.c. depends on time now
   typedef typename GFS::template ConstraintsContainer<Real>::Type CC;
   CC cc;
   Dune::PDELab::constraints( bctype, gfs, cc );
 
   // <<<3>>> Make instationary grid operator
   typedef Example03LocalOperator<BCTypeParam> LOP; 
-  LOP lop(bctype,4);                                           // local operator r
+  LOP lop(bctype,4);                                         // local operator r
   typedef Example03TimeLocalOperator TLOP; 
-  TLOP tlop(4);                                                 // local operator m
+  TLOP tlop(4);                                              // local operator m
   typedef Dune::PDELab::istl::BCRSMatrixBackend<> MBE;
   MBE mbe(25);
   typedef Dune::PDELab::GridOperator<GFS,GFS,LOP,MBE,Real,Real,Real,CC,CC> GO0;
@@ -33,7 +33,7 @@ void example03_Q2 (const GV& gv, double dt, double tend)
   typedef Dune::PDELab::GridOperator<GFS,GFS,TLOP,MBE,Real,Real,Real,CC,CC> GO1;
   GO1 go1(gfs,cc,gfs,cc,tlop,mbe);
   typedef Dune::PDELab::OneStepGridOperator<GO0,GO1> IGO;
-  IGO igo(go0,go1);                                             // new grid operator
+  IGO igo(go0,go1);                                          // new grid operator
 
   // How well did we estimate the number of entries per matrix row?
   // => print Jacobian pattern statistics (do not call this for IGO before osm.apply() was called!)
@@ -42,10 +42,10 @@ void example03_Q2 (const GV& gv, double dt, double tend)
 
   // <<<3>>> Make FE function with initial value / Dirichlet b.c.
   typedef typename IGO::Traits::Domain U;
-  U uold(gfs,0.0);                                              // solution at t^n
-  typedef BCExtension<GV,Real> G;                               // defines boundary condition,
-  G g(gv);                                                      // extension and initial cond.
-  g.setTime(time);                                              // b.c. depends on time now
+  U uold(gfs,0.0);                                           // solution at t^n
+  typedef BCExtension<GV,Real> G;                            // defines boundary condition,
+  G g(gv);                                                   // extension and initial cond.
+  g.setTime(time);                                           // b.c. depends on time now
   Dune::PDELab::interpolate(g,gfs,uold);
 
   // <<<5>>> Select a linear solver backend
@@ -57,27 +57,27 @@ void example03_Q2 (const GV& gv, double dt, double tend)
   PDESOLVER pdesolver(igo,ls,1e-10);
 
   // <<<7>>> time-stepper
-  Dune::PDELab::Alexander2Parameter<Real> method;               // defines coefficients
+  Dune::PDELab::Alexander2Parameter<Real> method;          // defines coefficients
   Dune::PDELab::OneStepMethod<Real,IGO,PDESOLVER,U,U> osm(method,igo,pdesolver);
-  osm.setVerbosityLevel(2);                                     // time stepping scheme
+  osm.setVerbosityLevel(2);                                   // time stepping scheme
 
   // <<<8>>> graphics for initial guess
-  Dune::PDELab::FilenameHelper fn("example03_Q2");              // append number to file name
+  Dune::PDELab::FilenameHelper fn("example03_Q2");            // append number to file name
   {
     Dune::SubsamplingVTKWriter<GV> vtkwriter(gv,3);
     Dune::PDELab::addSolutionToVTKWriter(vtkwriter,gfs,uold);
     vtkwriter.write(fn.getName(),Dune::VTK::appendedraw);
-    fn.increment();                                             // increase file number
+    fn.increment();                                           // increase file number
   }
 
   // <<<9>>> time loop
-  U unew(gfs,0.0);                                              // solution to be computed
+  U unew(gfs,0.0);                                            // solution to be computed
   while (time<tend-1e-8) {
       // do time step
-      bctype.setTime(time+dt);                                       // compute constraints
-      cc.clear();                                               // for this time step
+      bctype.setTime(time+dt);                                // compute constraints
+      cc.clear();                                             // for this time step
       Dune::PDELab::constraints(bctype,gfs,cc);
-      osm.apply(time,dt,uold,g,unew);                           // do one time step
+      osm.apply(time,dt,uold,g,unew);                         // do one time step
 
       // graphics
       Dune::SubsamplingVTKWriter<GV> vtkwriter(gv,3);
@@ -85,7 +85,7 @@ void example03_Q2 (const GV& gv, double dt, double tend)
       vtkwriter.write(fn.getName(),Dune::VTK::appendedraw);
       fn.increment();
 
-      uold = unew;                                              // advance time step
+      uold = unew;                                            // advance time step
       time += dt;
     }
 }
