b5cdcc2ac448a08ee4365a3132b3f428c034239b
Marian Piatkowski <marian.piatkowski@iwr.uni-heidelberg.de>
2015-07-01 16:11:10 +0200

    [Cleanup][NavierStokes] adapt constructor of local operators ...
    ... to new convention.
    You do not specify the integration order from the outside any more, but
    the superintegration order.
    The correct integration order is derived internally.

--- src/navier-stokes/cgstokes_instat.cc
+++ src/navier-stokes/cgstokes_instat.cc
@@ -66,7 +66,7 @@
 // The driver for all examples
 //===============================================================
 
-template<int q, typename GV, typename V_FEM, typename P_FEM, typename IF, typename PRM>
+template<int vOrder, typename GV, typename V_FEM, typename P_FEM, typename IF, typename PRM>
 void navierstokes
 (
  const GV& gv,
@@ -129,9 +129,9 @@ void navierstokes
 
   // Make grid function operator
   typedef Dune::PDELab::TaylorHoodNavierStokes<PRM> LOP;
-  LOP lop(parameters);
+  LOP lop(parameters,1);
   typedef Dune::PDELab::NavierStokesMass<PRM> MLOP;
-  MLOP mlop(parameters,q);
+  MLOP mlop(parameters,1);
 
   Dune::PDELab::FractionalStepParameter<RF> method;
   typedef Dune::PDELab::istl::BCRSMatrixBackend<> MBE;
@@ -214,7 +214,7 @@ void navierstokes
 
   // Compute norm of final solution
   typename PDGF::Traits::RangeType l1norm(0);
-  Dune::PDELab::integrateGridFunction(pdgf,l1norm,q);
+  Dune::PDELab::integrateGridFunction(pdgf,l1norm,2*vOrder+2);
   std::cout << std::setw(12) << std::setprecision(7) << std::scientific
             << "L1 norm: " <<  l1norm << std::endl;
 
@@ -296,7 +296,6 @@ int main(int argc, char** argv)
       typedef GridType::ctype DF;
       typedef double R;
       const int k=2;
-      const int q=2*k;
       typedef Dune::PDELab::PkLocalFiniteElementMap<GV,DF,R,k> V_FEM;
       typedef Dune::PDELab::PkLocalFiniteElementMap<GV,DF,R,k-1> P_FEM;
       V_FEM vFem(gv); P_FEM pFem(gv);
@@ -338,7 +337,7 @@ int main(int argc, char** argv)
          initial_solution,neumann_flux);
 
       // solve problem (Navier-Stokes equations)
-      navierstokes<q>
+      navierstokes<k>
         (gv,"turbtube_ug_P2P1_2d", parameters, config_parser, vFem, pFem, initial_solution);
     }
 #endif
@@ -369,7 +368,6 @@ int main(int argc, char** argv)
       typedef GridType::ctype DF;
       typedef double R;
       const int k=2;
-      const int q=2*k;
       typedef Dune::PDELab::PkLocalFiniteElementMap<GV,DF,R,k> V_FEM;
       typedef Dune::PDELab::PkLocalFiniteElementMap<GV,DF,R,k-1> P_FEM;
       V_FEM vFem(gv); P_FEM pFem(gv);
@@ -409,7 +407,7 @@ int main(int argc, char** argv)
          initial_solution,neumann_flux);
 
       // solve problem (Navier-Stokes equations)
-      navierstokes<q>
+      navierstokes<k>
         (gv,"lshape_ug_P2P1_2d", parameters, config_parser, vFem, pFem, initial_solution);
     }
 #endif
